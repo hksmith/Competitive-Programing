ca3b348957b949e2f1afd6379cd25c9391c1c1b3
kalab solomon <kalab27solomon@gmail.com>
2023-01-23 14:26:00 +0300

    adding fizzbuzz and grading students

---
+++ HackerRank/Grading Students.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    grd = []
+    for i in grades:
+        if i < 38:
+            grd.append(i)
+        else:
+            a = int(i / 5) + 1
+            b = a * 5
+            if (b - i < 3):
+                grd.append(b)
+            else:
+                grd.append(i)
+    return grd
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
